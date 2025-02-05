a45673f88097cf9553365e62c072a095827e1119
Rahul KUMAR <61095152+rahul-svg@users.noreply.github.com>
2025-02-05 13:55:28 +0530

    update portfolio (#1)

--- src/Component/data/skills.js
+++ src/Component/data/skills.js
@@ -1,6 +1,6 @@
 export const SkillsData = [
     {
-    type:"Frontend",
+    type:"Programming Languages",
     list:[
         {
           name:"HTML",
@@ -10,37 +10,90 @@ export const SkillsData = [
            name:"CSS",
            icon:<i class="devicon-css3-plain colored"></i> ,
         },
-
         {
-            name:"ReactJs",
-            icon:<i class="devicon-react-original colored"></i>  ,
-
-        },
-
-        {
-            name:"Bootstrap",
-            icon:<i class="devicon-bootstrap-plain colored"></i>  ,
-
-        },
-
-        {
-            name:"Javascript",
+            name:"Javascript (ES6+)",
             icon:<i class="devicon-javascript-plain colored"></i>  ,
+
+        },
+        {
+            name:"C++",
+            icon:<i class="devicon-cplusplus-plain colored"></i>,
 
         },
     ],
   },
+
   {
-      type:"Backend",
-      list:[
-          {
-              name:"C++",
-              icon:<i class="devicon-cplusplus-plain colored"></i>,
-          },
-          {
+    type:"Framework/Library",
+    list:[
+        {
+            name:"ReactJs",
+            icon:<i class="devicon-react-original colored"></i>  ,
+        
+        },
+        {
             name:"NodeJs",
             icon:<i class="devicon-nodejs-plain colored"></i>,
+
+        },
+        {
+          name:"JQuery",
+          icon:<i class="devicon-jquery-plain colored"></i>,
+        },
+        {
+            name:"Bootstrap",
+            icon:<i class="devicon-bootstrap-plain colored"></i>  ,
+
+        }, 
+        {
+            name:"Material UI",
+            icon:<i class="devicon-materialui-plain colored"></i>,
+
+        },
+        {
+            name:"Express",
+            icon:<i class="devicon-express-original colored"></i>,
+
+        },
+    ],
+  },
+
+  {
+      type:"Development/Design Tools",
+      list:[
+          {
+              name:"Webpack",
+              icon:<i class="devicon-webpack-plain colored"></i>,
+          },
+          {
+            name:"Git",
+            icon:<i class="devicon-git-plain colored"></i>,
+        },
+        {
+            name:"Bitbucket",
+            icon: <i class="devicon-bitbucket-original colored"></i>,
+        },
+        {
+            name:"Figma",
+            icon:<i class="devicon-figma-plain colored"></i>,
+        },
+        {
+            name:"Docker",
+            icon:<i class="devicon-docker-plain colored"></i>,
+        },
+        {
+            name:"Jenkins",
+            icon:<i class="devicon-jenkins-line colored"></i>,
         },
       ],
   },
+  {
+    type:"Database Tool",
+    list:[
+        {
+            name:"mySQL",
+            icon:<i class="devicon-mysql-original colored"></i>,
+        }
+    ],
+}
 ];
